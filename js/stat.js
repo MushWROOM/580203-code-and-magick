15bdee2dcc014c1fe15f759c0d7920e6e098b6f6
MushWROOM <muzzleface47@gmail.com>
2017-12-03 16:13:41 +0300

    add functions

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,71 @@
 window.renderStatistics = function (ctx, names, times) {
-	ctx.fillStyle = (0, 0, 0, 0.7);
+	var drawCloud = function (fillStyleR, fillStyleG, fillStyleB, fillStyleA, x0, y0){
+		ctx.fillStyle = (fillStyleR, fillStyleG, fillStyleB, fillStyleA);
+		ctx.beginPath();
+	    ctx.moveTo(x0, y0);
+		ctx.quadraticCurveTo(x0, y0 - 45, x0 + 70, y0 - 45);
+		ctx.quadraticCurveTo(x0 + 140, y0 - 90, x0 + 210, y0 - 45);
+		ctx.quadraticCurveTo(x0 + 280, y0 - 45, x0 + 280, y0);
+		ctx.quadraticCurveTo(x0 + 350, 155, x0 + 280, y0 + 90);
+		ctx.quadraticCurveTo(x0 + 280, y0 + 135, x0 + 210, y0 + 135);
+		ctx.quadraticCurveTo(x0 + 140, 290, x0 + 70, y0 + 135);
+		ctx.quadraticCurveTo(x0, y0 + 135, x0, y0 + 90);
+		ctx.quadraticCurveTo(x0 - 70, 155, x0, y0);
+		ctx.fill();
+	}
+	
+	var drawText = function (fillStyleHex, font, x0, y0, text) {
+		ctx.fillStyle = fillStyleHex;
+		ctx.font = font;
+		ctx.fillText = (text, x0, y0);
+	}
+	
+	var getMaxElement = function (blocks){
+	  var max = -1;
+	  for (var i = 0; i < blocks.length; i++){
+		  var element = blocks[i];
+		  if (element > max){
+			  max = element;
+		  }
+	  }
+	  return (max);
+	}
+	
+	var getRandomColor = function(name) {
+		if (name == 'Вы') {
+			ctx.fillStyle = (255, 0, 0, 1);
+		}
+		else {
+			var a = Math.random();
+			ctx.fillStyle = (0, 0, 255, a);
+		}
+	}	
+	
+	var drawHistogram = function (histogramHeight, times, names, x0, width, maxValue){
+	  var step = histogramHeight / (maxValue - 0);
+	
+	  for (var i = 0; i < times.length; i++){
+		  getRandomColor(names[i]);
+		  var x = x0 + i * width;
+		  var y = times[i] * step;
+		  ctx.beginPath;
+		  ctx.moveTo(x, 200);
+		  ctx.lineTo(x, 200 - y);
+		  ctx.lineTo(x + 40, 200 - y);
+		  ctx.lineTo(x + 40, 200);
+		  ctx.closePath();
+		  ctx.fillText(names[i], x - 30, 180);
+		  ctx.fillText(Math.round(times[i]), x - 30, 200 - y - 20); 
+	    }
+	}
+	
+	
+	drawCloud(0, 0, 0, 0.7, 180, 110); // shadow
+	drawCloud(255, 255, 255, 1, 170, 100); // cloud
+	
+	
+	
+	/*ctx.fillStyle = (0, 0, 0, 0.7);
 	ctx.beginPath();
 	ctx.moveTo(180, 110);
 	ctx.quadraticCurveTo(180, 65, 250, 65);
@@ -25,34 +91,25 @@ window.renderStatistics = function (ctx, names, times) {
 	ctx.quadraticCurveTo(170, 235, 170, 190);
 	ctx.quadraticCurveTo(100, 145, 170, 100);
 	ctx.fill();
+	*/
 	
-	ctx.fillStyle = '#000';
+	drawText = ('#000', '16 px PT Mono', 230, 70, 'Ура, вы победили!');
+	drawText = ('#000', '16 px PT Mono', 230, 85, 'Список результатов: ');
+	
+	/*ctx.fillStyle = '#000';
 	ctx.font = '16 px PT Mono';
 	
 	ctx.fillText ('Ура, вы победили!', 230, 70);
 	ctx.fillText ('Список результатов: ', 230, 85);
+	*/
+	var maxValue = getMaxElement(times);
 	
-	var max = -1;
-	var maxIndex = -1;
 	
-	for (var i = 0; i < times.length; i++){
-		var time = times[i];
-		if (time > max){
-			max = time;
-			maxIndex = i;
-		}
-	}
-	var histogramHeight = 150;
-	var step = histogramHeight / (max - 0);
-	var colors = ['blue', 'green', 'red', 'black'];
+	/*var histogramHeight = 150;
+	var step = histogramHeight / (maxValue - 0);
 	
 	for (var i = 0; i < times.length; i++){
-		if (names[i] === 'Вы') {
-			ctx.fillStyle = (255, 0, 0, 1);
-		}
-		else {
-			ctx.fillStyle = (0, 0, 255, Math.random());
-		}
+		getRandomColor(names[i]);
 		var x = 200 + i * 90;
 		var y = times[i] * step;
 		ctx.beginPath;
@@ -63,6 +120,8 @@ window.renderStatistics = function (ctx, names, times) {
 		ctx.closePath();
 		ctx.fillText(names[i], x - 30, 180);
 		ctx.fillText(Math.round(times[i]), x - 30, 200 - y - 20); 
-	}
+	}*/
+	drawHistogram(150, times, names, 200, 90, maxValue);
 }
+
 	
