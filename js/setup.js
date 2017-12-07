afaea588e1bd331cfe1b14c0f73e54e08192a614
MushWROOM <muzzleface47@gmail.com>
2017-12-07 09:16:01 +0300

    Сделал правки.

--- js/setup.js
+++ js/setup.js
@@ -8,12 +8,16 @@ userDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+var getRandomElement = function(arrayElements) {
+  var randomElement = arrayElements[Math.floor(Math.random() * arrayElements.length)];
+  return randomElement;
+};
 var createWizards = function (name, surname, coatColor, eyesColor) {
   var wizards = [];
   for (var i = 0; i < 4; i++) {
-    var fullname = name[Math.floor(Math.random() * name.length)] + ' ' + surname[Math.floor(Math.random() * surname.length)];
-    var wizardCoatColor = coatColor[Math.floor(Math.random() * coatColor.length)];
-    var wizardEyesColor = eyesColor[Math.floor(Math.random() * eyesColor.length)];
+    var fullname = getRandomElement(name) + ' ' + getRandomElement(surname);
+    var wizardCoatColor = getRandomElement(coatColor);
+    var wizardEyesColor = getRandomElement(eyesColor);
     wizards[i] = {
       name: fullname,
       coat: wizardCoatColor,
@@ -22,17 +26,17 @@ var createWizards = function (name, surname, coatColor, eyesColor) {
   }
   return wizards;
 };
-var getWizard = function (wizard) {
+var createOneNewWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coat;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyes;
   return wizardElement;
 };
-var appendFragment = function (wizard) {
+var appendFragment = function (wizards) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizard.length; i++) {
-    fragment.appendChild(getWizard(wizard[i]));
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(createOneNewWizard(wizards[i]));
   }
   return fragment;
 };
